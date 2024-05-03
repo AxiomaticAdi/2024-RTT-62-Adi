1888abf25532ba8210afef62f1d01c64315cd240
AxiomaticAdi <arudra@bu.edu>
2024-05-02 19:44:24 -0700

    PA 303.2.2

---
+++ PA_303_2_2/src/main/java/PerScholas/HackRankProblem1.java
@@ -0,0 +1,16 @@
+package PerScholas;
+
+import java.util.Scanner;
+
+public class HackRankProblem1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
