be9abc06e4504fecbf4fe9e8afbb564ce37d3078
AxiomaticAdi <arudra@bu.edu>
2024-05-07 21:27:49 -0700

    Remake getters and setters with intellij

--- java-homework/GLAB_303_9_1/src/main/java/PerScholas/HumanBeing.java
+++ java-homework/GLAB_303_9_1/src/main/java/PerScholas/HumanBeing.java
@@ -17,18 +17,23 @@ public class HumanBeing {
     public float getWeight() {
         return weight;
     }
+
     public void setWeight(float weight) {
         this.weight = weight;
     }
+
     public float getHeight() {
         return height;
     }
+
     public void setHeight(float height) {
         this.height = height;
     }
+
     public float getBmi() {
         return bmi;
     }
+
     public void setBmi(float bmi) {
         this.bmi = bmi;
     }
