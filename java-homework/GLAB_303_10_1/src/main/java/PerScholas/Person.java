31525ba08960c786bec1d6aacc004f186f060516
AxiomaticAdi <arudra@bu.edu>
2024-05-09 17:35:34 -0700

    Glab upload

---
+++ java-homework/GLAB_303_10_1/src/main/java/PerScholas/Person.java
@@ -0,0 +1,53 @@
+package PerScholas;
+
+public class Person {
+    String name;
+    static int lifeSpan = 60;
+    static double ageFactor = 1.0;
+    public Person() {
+        name = "";
+    }
+    public Person(String aName) {
+        name = aName;
+    }
+    public String getName() { return name; }
+    public void setName(String aName) { name = aName; }
+    public String toString() {
+        return("Hello, my name is " + name);
+    }
+    public String talk() {
+        return("I have nothing to say.");
+
+    }
+    public String walk() {
+        return("I have nowhere to go.");
+    }
+    public static double lifeSpan() {
+        return(lifeSpan * ageFactor);
+    }}
+
+class Boy extends Person {
+    static double ageFactor = 1.1;
+    public String talk() {
+        return(super.talk() + " ... but I love Java class.");
+    }
+    public String walk() {
+        return("I am now walking");
+    }
+}
+
+class Girl extends Person {
+    static double ageFactor = 1.3;
+    public Girl(String aName) {
+        name = "Ms." + aName;
+    }
+    public String talk() {
+        return("Hello! " + jump());
+    }
+    public String jump() {
+        return("I am jumping.");
+    }
+    public static double lifeSpan() {
+        return(lifeSpan * ageFactor);
+    }
+}
